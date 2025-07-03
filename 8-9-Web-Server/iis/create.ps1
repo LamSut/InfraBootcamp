0d2d4aca1401a28f61398dd04094122f40c4233a
LamSut <lamsut0007@gmail.com>
2025-07-03 16:07:51 +0700

    nilhil

--- 8-9-Web-Server/iis/create.ps1
+++ 8-9-Web-Server/iis/create.ps1
@@ -3,5 +3,8 @@ powershell -ExecutionPolicy Bypass -File .\setup\lim2.ps1
 powershell -ExecutionPolicy Bypass -File .\setup\limdotnet.ps1
 
 Copy-Item "..\lim1.html" "C:\inetpub\lim1\lim1.html" -Force
+Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.webServer/defaultDocument/files" -name "." -value @{value='lim1.html'} -location "Lim1"
+
 Copy-Item "..\lim2.html" "C:\inetpub\lim2\lim2.html" -Force
+Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.webServer/defaultDocument/files" -name "." -value @{value='lim2.html'} -location "Lim2"
 
--- 8-9-Web-Server/iis/setup/lim1.ps1
+++ 8-9-Web-Server/iis/setup/lim1.ps1
@@ -11,7 +11,3 @@ if (-Not (Test-Path "IIS:\AppPools\Lim1")) {
 if (-Not (Get-Website -Name "Lim1" -ErrorAction SilentlyContinue)) {
     New-Website -Name "Lim1" -Port 80 -HostHeader "lim1.local" -PhysicalPath "C:\inetpub\lim1" -ApplicationPool "Lim1"
 }
-
-Copy-Item ".\lim1.html" "C:\inetpub\lim1\lim1.html" -Force
-
-Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.webServer/defaultDocument/files" -name "." -value @{value='lim1.html'} -location "Lim1"
