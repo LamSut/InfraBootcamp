2b12cb56e22d1bab6130c6cb1de6b7276496e288
LamSut <lamsut0007@gmail.com>
2025-07-04 11:57:10 +0700

    lab 11 backup local 1st

--- 11-Shell-Script/backup.sh
+++ 11-Shell-Script/backup.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+mkdir -p ./backup/src
+tar -czf ./backup/src/src-$(date +%F-%H%M%S).tar.gz -C ./repo .
+
+mkdir -p ./backup/db
+sudo mysqldump -u root --routines --triggers --all-databases > ./backup/db/db-$(date +%F-%H%M%S).sql
