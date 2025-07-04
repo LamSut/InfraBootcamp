6ae8bc31592e96ed12b8c824303a59ac2c9a5e92
LamSut <lamsut0007@gmail.com>
2025-07-04 16:26:01 +0700

    remove unused contents

--- 6-IAM/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# aws iam delete-login-profile --user-name iamlim1
-# aws iam delete-login-profile --user-name iamlim2
-
-terraform init
-terraform plan
-terraform destroy --auto-approve
