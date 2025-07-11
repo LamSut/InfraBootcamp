49ff21c63ad62df56aca3c956d96bf3fd8d57c4a
LamSut <lamsut0007@gmail.com>
2025-07-11 09:29:22 +0700

    add cmt

--- Test/drupal-docker/create.sh
+++ Test/drupal-docker/create.sh
@@ -1,5 +1,6 @@
 if ! sudo grep -q "limdrupal.local" /etc/hosts; then
     echo "127.0.0.1 limdrupal.local" | sudo tee -a /etc/hosts > /dev/null
+    echo "Added limdrupal.local to /etc/hosts"
 fi
 
 mkdir -p certs
@@ -7,6 +8,7 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout certs/privkey.pem \
   -out certs/fullchain.pem \
   -subj "/C=VN/ST=CT/L=CT/O=BE/OU=BECT/CN=limdrupal.local"s
+echo "Generated self-signed SSL certificate"
 
 docker compose up -d
 
